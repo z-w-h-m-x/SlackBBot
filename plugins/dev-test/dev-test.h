753c9b28eb06cb4880c6abf367af565c8ba6237a
z-w-h-m-x <2159333498@qq.com>
2024-09-09 13:15:19 +0800

    sync

--- plugins/dev-test/dev-test.h
+++ plugins/dev-test/dev-test.h
@@ -1,5 +1,5 @@
-#include "Plugin/Plugin_Base.h"
-//实际使用是 "QNTBOT/Plugin_Base.h" （前提是导入编译后导出的include目录）
-// The actual use is "QNTBOT/Plugin_Base.h" (if you import the compiled include directory) （Machine translation）
+#include "Plugin/Plugin/Plugin_Base.h"
+//实际使用是 "SlackBot/Plugin_Base.h" （前提是导入编译后导出的include目录）
+// The actual use is "SlackBot/Plugin_Base.h" (if you import the compiled include directory) （Machine translation）
 
 Plugin_Extern PluginType SetPluginType();
